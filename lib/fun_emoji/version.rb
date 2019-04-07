12eca9110e699cc02ad4714f73f59aa55fea8973
Srinidhi G S <srini.satti+lwt@gmail.com>
2019-04-08 01:01:46 +0530

    Fun emoji initial commit

---
+++ lib/fun_emoji/version.rb
@@ -0,0 +1,4 @@
+module FunEmoji
+  VERSION = "0.5.0"
+end
+
---
+++ spec/fun_emoji_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe FunEmoji do
+  it "has a version number" do
+    expect(FunEmoji::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
